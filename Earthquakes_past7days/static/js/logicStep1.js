feefd6c06ed5d1389b0acee4a3e4837025228b66
Kmkinnear <korykinnear@triad.rr.com>
2022-12-10 19:03:01 -0500

    updated

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -28,36 +28,6 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the airport GeoJSON URL
-//let airportData = "https://raw.githubusercontent.com/kmkinnear/Mapping_Earthquakes/main/majorAirports.json";
-
-// Grabbing our GeoJSON data.
-/*d3.json(airportData).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
-});*/
-
-// Accessing the Toronto airline routes GeoJSON URL.
-/*let torontoData = "https://raw.githubusercontent.com/kmkinnear/Mapping_Earthquakes/main/torontoRoutes.json";
-
-// Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-  color: "#ffffa1",
-  weights: 2,
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
-    + feature.properties.dst + "</h3>");
-  }
-}).addTo(map);
-});*/
-
-// Accessing the Toronto neighborhoods GeoJSON URL.
-//let torontoHoods = "https://raw.githubusercontent.com/kmkinnear/Mapping_Earthquakes/main/torontoNeighborhoods.json";
-
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   console.log(data);
   L.geoJSON(data).addTo(map);
